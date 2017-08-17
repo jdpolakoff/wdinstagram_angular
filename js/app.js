648ee1ab956525e91060b13e4684d274b7d59b05
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-17 13:15:50 -0400

    full CRU functionality

--- js/app.js
+++ js/app.js
@@ -17,6 +17,21 @@ angular
   'Insta',
   InstaIndexControllerFunction
 ])
+.controller('InstaNewController', [
+  '$state',
+  'Insta',
+  InstaNewControllerFunction
+])
+.controller('InstaShowController', [
+  '$stateParams',
+  'Insta',
+  InstaShowControllerFunction
+])
+.controller('InstaEditController', [
+  '$state',
+  'Insta',
+  InstaEditControllerFunction
+])
 
 
 function instaService ($resource) {
@@ -35,8 +50,48 @@ function RouterFunction ($stateProvider) {
     controller: 'InstaIndexController',
     controllerAs: 'vm'
   })
+  .state('instaNew', {
+        parent: 'instaIndex',
+        url: '/new',
+        templateUrl: 'js/instagrams/new.html',
+        controller: 'InstaNewController',
+        controllerAs: 'vm'
+      })
+  .state('instaShow', {
+    url: '/instagrams/:id',
+    templateUrl: 'js/instagrams/show.html',
+    controller: 'InstaShowController',
+    controllerAs: 'vm'
+  })
+  .state('instaEdit', {
+      parent: 'instaShow',
+      url: '/edit',
+      templateUrl: 'js/instagrams/edit.html',
+      controller: 'InstaEditController',
+      controllerAs: 'vm'
+    })
 }
 
 function InstaIndexControllerFunction (Insta) {
   this.instagrams = Insta.query()
+}
+
+function InstaShowControllerFunction ($stateParams, Insta) {
+  this.instagram = Insta.get({id: $stateParams.id})
+}
+
+function InstaNewControllerFunction ($state, Insta) {
+  this.newInsta = new Insta()
+  this.create = function () {
+    this.newInsta.$save(() => {
+      $state.go('instaIndex', {}, { reload: true })
+    })
+  }
+}
+
+function InstaEditControllerFunction ($state, Insta) {
+  this.insta = Insta.get({id: $state.params.id})
+  this.update = function () {
+    this.insta.$update({id: $state.params.id})
+  }
 }
