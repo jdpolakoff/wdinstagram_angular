9d088b91ba10d5f06d92068f3c1069f1b48676f2
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-16 22:02:18 -0400

    got index page up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+angular
+.module("wdinstagram", [
+    'ui.router',
+    'ngResource'
+  ])
+.config([
+  '$stateProvider',
+  RouterFunction
+])
+.factory('Insta', [
+  '$resource',
+  instaService
+])
+.controller('InstaIndexController', [
+  'Insta',
+  InstaIndexControllerFunction
+])
+
+
+function instaService ($resource) {
+  return $resource('http://localhost:3000/entries/:id', {}, {
+    update: {
+      method: 'PUT'
+    }
+})
+}
+
+function RouterFunction ($stateProvider) {
+  $stateProvider
+  .state('instaIndex', {
+    url: '/instagrams',
+    templateUrl: 'js/instagrams/index.html',
+    controller: 'InstaIndexController',
+    controllerAs: 'vm'
+  })
+}
+
+function InstaIndexControllerFunction (Insta) {
+  this.instagrams = Insta.query()
+}
